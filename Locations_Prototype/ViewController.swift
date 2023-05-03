53fdd71f134fc7d6e18f2ce6acceb12cde86585f
Christian.Starly <yoh.chrisitan@gmail.com>
2023-05-03 13:49:46 -0400

    Initial Commit

---
+++ Locations_Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locations_Prototype
+//
+//  Created by Christian Calixto on 3/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
